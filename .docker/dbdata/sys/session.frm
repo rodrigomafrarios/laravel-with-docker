7811940f490276de9b5fbc7d142e09ec79673b03
Rodrigo <rodrigomafrarios@gmail.com>
2020-01-07 08:37:23 -0200

    configurando phpunit

--- .docker/dbdata/sys/session.frm
+++ .docker/dbdata/sys/session.frm
@@ -7,7 +7,7 @@ definer_user=mysql.sys
 definer_host=localhost
 suid=0
 with_check_option=0
-timestamp=2019-09-28 15:21:23
+timestamp=2020-01-03 10:30:29
 create-version=1
 source=SELECT * FROM sys.processlist WHERE conn_id IS NOT NULL AND command != \'Daemon\'
 client_cs_name=utf8
--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,7 +4,6 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Ramsey\Uuid\Uuid;
 
 class Category extends Model
 {
@@ -13,4 +12,5 @@ class Category extends Model
     protected $fillable = array('name','description','is_active');
     protected $dates = array('deleted_at');
     protected $casts = array('id' => 'string');
+    public $incrementing = false;
 }
